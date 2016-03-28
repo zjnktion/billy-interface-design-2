d23df91e5bed434e69139d69119db0ae1357022b
zjnktion <zjnktion@163.com>
2016-03-28 16:30:30 +0800

    [billy]BiFuture comment.

--- billy-core/src/main/java/cn/zjnktion/billy/future/BiFuture.java
+++ billy-core/src/main/java/cn/zjnktion/billy/future/BiFuture.java
@@ -3,7 +3,6 @@ package cn.zjnktion.billy.future;
 import cn.zjnktion.billy.context.BiContext;
 import cn.zjnktion.billy.listener.BiFutureListener;
 
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -12,23 +11,69 @@ import java.util.concurrent.TimeUnit;
 public interface BiFuture {
 
     /**
-     * Returns a {@link BiContext} where the I/O operation associated with this future task take place
+     * Adds the specified listener to this future.
+     * The specified listener is notified after this future is {@linkplain #isCompleted() completed}.
+     * If this future is already completed before added, the specified listener is notified immediately.
+     *
+     * @param listener The listener to add
+     * @return the current {@link BiFuture}
+     */
+    BiFuture addListener(BiFutureListener<?> listener);
+
+    /**
+     * Removes the specified listener from this future.
+     * The specified listener is no longer notified when this future is {@linkplain #isCompleted() completed}.
+     * If the specified listener is not associated with this future, this method does nothing and returns silently.
+     *
+     * @param listener The listener to remove
+     * @return the current {@link BiFuture}
+     */
+    BiFuture removeListener(BiFutureListener<?> listener);
+
+    /**
+     * Wait for this future to be completed.
+     *
+     * @return the current {@link BiFuture}
+     * @throws InterruptedException If the current thread was interrupted
+     */
+    BiFuture await() throws InterruptedException;
+
+    /**
+     * Wait for this future to be completed within the specified time limit.
+     *
+     * @param timeout
+     * @param unit
+     * @return {@code true} if an only if the future was completed within the specified time limit
+     * @throws InterruptedException If the current thread was interrupted
+     */
+    boolean await(long timeout, TimeUnit unit) throws InterruptedException;
+
+    /**
+     * Wait for this future to be completed without {@link InterruptedException}.
+     * This method catches an {@link InterruptedException} and discards it silently.
+     *
      * @return
+     */
+    BiFuture awaitUninterruptibly();
+
+    /**
+     * Wait for this future to be completed within the specified time limit without {@link InterruptedException}.
+     * This method catches an {@link InterruptedException} and discards it silently.
+     *
+     * @param timeout
+     * @param unit
+     * @return {@code true} if and only if the future was completed within the specified time limit
+     */
+    boolean awaitUninterruptibly(long timeout, TimeUnit unit);
+
+    /**
+     * @return a {@link BiContext} where the I/O operation associated with this future takes place.
      */
     BiContext getContext();
 
-    BiFuture addListener(BiFutureListener<?> listener);
-
-    BiFuture removeListener(BiFutureListener<?> listener);
-
-    BiFuture await() throws InterruptedException;
-
-    boolean await(long timeout, TimeUnit unit) throws InterruptedException;
-
-    BiFuture awaitUninterruptibly();
-
-    boolean awaitUninterruptibly(long timeout, TimeUnit unit);
-
+    /**
+     * @return {@code true} if this future is completed
+     */
     boolean isCompleted();
 
 }
