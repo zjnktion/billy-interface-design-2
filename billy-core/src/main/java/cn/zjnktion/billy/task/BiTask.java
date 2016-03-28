3c1500d112e7a5b87651e3802e86bba59ee2c791
zjnktion <zjnktion@163.com>
2016-03-28 18:03:43 +0800

    [billy]task design

--- billy-core/src/main/java/cn/zjnktion/billy/task/BiTask.java
+++ billy-core/src/main/java/cn/zjnktion/billy/task/BiTask.java
@@ -1,7 +1,21 @@
 package cn.zjnktion.billy.task;
 
+import cn.zjnktion.billy.future.BiFuture;
+
 /**
+ * Design for designing, maybe i should remove this interface from billy.
  * Created by zhengjn on 2016/3/23.
  */
 public interface BiTask {
+
+    /**
+     * @return the original task which is not filtered by any {@link cn.zjnktion.billy.filter.BiFilter}
+     */
+    BiTask getOriginalTask();
+
+    /**
+     * @return the future associate this task
+     */
+    BiFuture getFuture();
+
 }
--- billy-core/src/main/java/cn/zjnktion/billy/task/WriteTask.java
+++ billy-core/src/main/java/cn/zjnktion/billy/task/WriteTask.java
@@ -1,7 +1,22 @@
 package cn.zjnktion.billy.task;
 
+import cn.zjnktion.billy.future.WriteFuture;
+
+import java.net.SocketAddress;
+
 /**
  * Created by zhengjn on 2016/3/23.
  */
 public interface WriteTask extends BiTask {
+
+    boolean isEncode();
+
+    Object getMessage();
+
+    SocketAddress getDestination();
+
+    WriteTask getOriginalTask();
+
+    WriteFuture getFuture();
+
 }
